e0f114e7c00d2ae0c2eff2857f80b18e04804009
Joseph Maramba <josephmaramba50@gmail.com>
2022-12-12 13:34:46 +0300

    working on navbar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      width: {
+        fluid: "min(100% - 2rem, 110rem)",
+      },
+	  fontFamily: {
+		
+	  }
+    },
+  },
+  plugins: [],
+};
